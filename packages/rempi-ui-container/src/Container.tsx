095a823da09a0c7060af6a0265365321bc14eb99
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-07-03 16:33:59 -0300

    feat: add container component

---
+++ packages/rempi-ui-container/src/Container.tsx
@@ -0,0 +1,36 @@
+import { HTMLRempiProps } from "@rempi-ui/core";
+import classnames from "classnames";
+import { forwardRef } from "react";
+import "./Container.scss";
+
+type ContainerVariant = "sm" | "md" | "lg" | "xlg" | "2xlg" | "fluid";
+const containerVariantClasses: Record<ContainerVariant, string> = {
+  sm: "rempi-container--sm",
+  md: "rempi-container--md",
+  lg: "rempi-container--lg",
+  xlg: "rempi-container--xlg",
+  "2xlg": "rempi-container--2xlg",
+  fluid: "rempi-container--fluid",
+};
+
+export type ContainerProps = HTMLRempiProps<"div"> & {
+  variant?: ContainerVariant;
+  centered?: boolean;
+};
+
+export const Container = forwardRef<"div", ContainerProps>(
+  ({ children, variant, centered = true, as: Component = "div", ...props }, ref) => {
+    return (
+      <Component
+        {...props}
+        ref={ref}
+        className={`rempi-container ${classnames(
+          variant && containerVariantClasses[variant],
+          { "rempi-container--centered": centered }
+        )}`}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
