bc6f2221d999baa2bf32cbe2c20fab364e690bde
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-07-03 16:35:49 -0300

    feat: add container to main page

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,56 +1,61 @@
 import { Button } from "@rempi-ui/button";
+import { Container } from "@rempi-ui/container";
 import { Flex } from "@rempi-ui/flex";
 import { Heading } from "@rempi-ui/heading";
 import { Typography } from "@rempi-ui/typography";
 
 export default function Page() {
   return (
-    <Flex direction="column" alignItems="center">
-      <Button
-        as="a"
-        href="https://google.com.br"
-        color="primary"
-        variant="contained"
-        buttonSize="standard"
-      >
-        Hello
-      </Button>
-      <Button
-        as="div"
-        color="secondary"
-        variant="contained"
-        buttonSize="standard"
-      >
-        Hello
-      </Button>
-      <Button color="secondary" variant="contained" buttonSize="standard">
-        Hello
-      </Button>
-      <Typography variant="lead1">Typography 1</Typography>
-      <Typography variant="lead2">Typography 2</Typography>
-      <Typography variant="body1">Typography 3</Typography>
-      <Typography variant="body2">Typography 4</Typography>
-      <Typography variant="caption">Typography 5</Typography>
-      <Typography variant="subtitle">Typography 6</Typography>
+    <Container variant="md" style={{
+      backgroundColor: 'var(--colors-secondary2)'
+    }}>
+      <Flex direction="column" alignItems="center">
+        <Button
+          as="a"
+          href="https://google.com.br"
+          color="primary"
+          variant="contained"
+          buttonSize="standard"
+        >
+          Hello
+        </Button>
+        <Button
+          as="div"
+          color="secondary"
+          variant="contained"
+          buttonSize="standard"
+        >
+          Hello
+        </Button>
+        <Button color="secondary" variant="contained" buttonSize="standard">
+          Hello
+        </Button>
+        <Typography variant="lead1">Typography 1</Typography>
+        <Typography variant="lead2">Typography 2</Typography>
+        <Typography variant="body1">Typography 3</Typography>
+        <Typography variant="body2">Typography 4</Typography>
+        <Typography variant="caption">Typography 5</Typography>
+        <Typography variant="subtitle">Typography 6</Typography>
 
-      <Heading as="h1" variant="1">
-        Heading 1
-      </Heading>
-      <Heading as="h2" variant="2">
-        Heading 2
-      </Heading>
-      <Heading as="h3" variant="3">
-        Heading 3
-      </Heading>
-      <Heading as="h4" variant="4">
-        Heading 4
-      </Heading>
-      <Heading as="h5" variant="5">
-        Heading 5
-      </Heading>
-      <Heading as="h6" variant="6">
-        Heading 6
-      </Heading>
-    </Flex>
+        <Heading as="h1" variant="1">
+          Heading 1
+        </Heading>
+        <Heading as="h2" variant="2">
+          Heading 2
+        </Heading>
+        <Heading as="h3" variant="3">
+          Heading 3
+        </Heading>
+        <Heading as="h4" variant="4">
+          Heading 4
+        </Heading>
+        <Heading as="h5" variant="5">
+          Heading 5
+        </Heading>
+        <Heading as="h6" variant="6">
+          Heading 6
+        </Heading>
+      </Flex>
+    </Container>
   );
 }
