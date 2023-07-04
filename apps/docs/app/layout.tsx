4527cf1ead42d49a16e011eb45a8c8940e8e3427
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-07-04 09:49:04 -0300

    feat: add accordion

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,25 +1,28 @@
 import { createTheme, rempiDefaultConfig } from "@rempi-ui/core";
+import { Inter } from "next/font/google";
+import "@rempi-ui/core/src/config/global-reset.scss";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  const { className, getCssText } = createTheme('dark-theme', rempiDefaultConfig)
+  const { className, getCssText } = createTheme(
+    "dark-theme",
+    rempiDefaultConfig
+  );
 
   return (
-    <html lang="en" className={className}>
+    <html lang="en" className={`${className} ${inter.className}`}>
       <head>
-        <style id="#rempi" dangerouslySetInnerHTML={{ __html: getCssText() }}></style>
+        <style
+          id="#rempi"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        ></style>
       </head>
-      <body
-        style={{
-          color: 'var(--colors-error12)',
-          backgroundColor: 'var(--colors-primary1)',
-        }}>
-          {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,14 +1,75 @@
+"use client";
+
 import { Button } from "@rempi-ui/button";
 import { Container } from "@rempi-ui/container";
 import { Flex } from "@rempi-ui/flex";
 import { Heading } from "@rempi-ui/heading";
 import { Typography } from "@rempi-ui/typography";
+import { Accordion } from "@rempi-ui/accordion";
 
 export default function Page() {
   return (
-    <Container variant="md" style={{
-      backgroundColor: 'var(--colors-secondary2)'
-    }}>
+    <Container variant="md">
+      <Accordion.Root type="multiple" defaultValue={["item-1"]}>
+        <Accordion.Item value="item-1">
+          <Accordion.Header>
+            <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </Accordion.Content>
+        </Accordion.Item>
+
+        <Accordion.Item value="item-2">
+          <Accordion.Header>
+            <Accordion.Trigger>Is it unstyled?</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            Yes. It's unstyled by default, giving you freedom over the look and
+            feel.
+          </Accordion.Content>
+        </Accordion.Item>
+
+        <Accordion.Item value="item-3">
+          <Accordion.Header>
+            <Accordion.Trigger>Can it be animated?</Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            Yes! You can animate the Accordion with CSS or JavaScript.
+          </Accordion.Content>
+        </Accordion.Item>
+
+        <Accordion.Item value="item-4">
+          <Accordion.Header>
+            <Accordion.Trigger>
+              How long the content could be?
+            </Accordion.Trigger>
+          </Accordion.Header>
+          <Accordion.Content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            commodo justo ut tempus mollis. Praesent tincidunt vitae sapien
+            varius mollis. Nunc non felis non dui hendrerit ornare. Integer
+            volutpat congue eros id ultricies. Aenean viverra sagittis sapien,
+            nec cursus turpis egestas sit amet. Nunc nec pulvinar turpis, in
+            consectetur sapien. Aenean scelerisque neque vel tortor
+            pellentesque, quis vulputate erat gravida. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Suspendisse quam arcu, porta
+            vitae risus in, convallis ultricies sapien. Quisque feugiat egestas
+            congue. Vestibulum ac porttitor erat, sed blandit sem. Phasellus
+            lacus urna, tincidunt et efficitur et, luctus eget ligula. Quisque
+            dictum placerat ante, sed dapibus felis volutpat quis. Nunc dolor
+            sem, tincidunt vel ultrices in, feugiat et nisi. Morbi augue tellus,
+            malesuada id accumsan quis, eleifend ac elit. Aliquam pretium diam
+            quis nisi consectetur fringilla. Curabitur varius augue sed justo
+            auctor dignissim. Maecenas quis lacus non leo varius rhoncus.
+            Pellentesque rhoncus lectus a vehicula mattis. Donec nec lorem eros.
+            Nulla vitae libero malesuada, pellentesque orci et, ornare dolor.
+            Etiam mollis augue purus, in dignissim eros fringilla id. Etiam
+            metus urna, tincidunt sed nibh eu, semper rhoncus ante. Morbi
+            laoreet nisi hendrerit, ullamcorper nisl at, ornare mi. Nulla ut.
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
       <Flex direction="column" alignItems="center">
         <Button
           as="a"
