b3d87be50beb70157d4c706b1638dbca32650840
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-07-13 13:29:39 -0300

    feat: add separator component

---
+++ packages/rempi-ui-separator/src/Separator.tsx
@@ -0,0 +1,19 @@
+import * as SeparatorRadix from "@radix-ui/react-separator";
+import { HTMLRempiProps } from "@rempi-ui/core";
+import classnames from "classnames";
+import { forwardRef } from "react";
+import "./Separator.scss";
+
+export type SeparatorProps = HTMLRempiProps<typeof SeparatorRadix.Root>;
+
+export const Separator = forwardRef<typeof SeparatorRadix.Root, SeparatorProps>(
+  ({ className, as: Component = SeparatorRadix.Root, ...props }, ref) => {
+    return (
+      <Component
+        {...props}
+        ref={ref}
+        className={`rempi-separator ${classnames(className)}`}
+      />
+    );
+  }
+);
