12af238a33430e76cce2c20f1c7cc0818f316a44
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-07-21 16:49:16 -0300

    feat: using navigation menu

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -40,9 +40,28 @@ import { Input } from "@rempi-ui/input";
 import { Textarea } from "@rempi-ui/textarea";
 import { Menubar } from "@rempi-ui/menubar";
 import { Toast } from "@rempi-ui/toast";
+import { NavigationMenu } from "@rempi-ui/navigation-menu";
+import { forwardRef, HTMLRempiProps } from "@rempi-ui/core";
 
 const RADIO_ITEMS = ["Andy", "Benoît", "Luis"];
 const CHECK_ITEMS = ["Always Show Bookmarks Bar", "Always Show Full URLs"];
+
+type ListItemProps = HTMLRempiProps<"a"> & { title: string };
+
+const ListItem = forwardRef<"a", ListItemProps>(
+  ({ className, title, children, ...props }, ref) => {
+    return (
+      <li>
+        <NavigationMenu.Link asChild>
+          <a className={`list-item-link ${className}`} {...props} ref={ref}>
+            <div className="list-item-heading">{title}</div>
+            <p className="list-item-text">{children}</p>
+          </a>
+        </NavigationMenu.Link>
+      </li>
+    );
+  }
+);
 
 export default function Page() {
   const { availableThemes, setTheme } = useContext(ThemeProviderContext);
@@ -101,6 +120,138 @@ export default function Page() {
         </Select.Root>
       </Flex>
 
+      <Separator className="margin-bt-8" />
+
+      <NavigationMenu.Root>
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Learn</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <ul className="list one">
+              <li style={{ gridRow: "span 3 / auto" }}>
+                <NavigationMenu.Link asChild>
+                  <a className="callout" href="/">
+                    <svg
+                      aria-hidden
+                      width="38"
+                      height="38"
+                      viewBox="0 0 25 25"
+                      fill="var(--colors-grey12)"
+                    >
+                      <path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
+                      <path d="M12 0H4V8H12V0Z"></path>
+                      <path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
+                    </svg>
+                    <div className="callout-heading">Radix Primitives</div>
+                    <p className="callout-text">
+                      Unstyled, accessible components for React.
+                    </p>
+                  </a>
+                </NavigationMenu.Link>
+              </li>
+
+              <ListItem href="https://stitches.dev/" title="Stitches">
+                CSS-in-JS with best-in-class developer experience.
+              </ListItem>
+              <ListItem href="/colors" title="Colors">
+                Beautiful, thought-out palettes with auto dark mode.
+              </ListItem>
+              <ListItem href="https://icons.radix-ui.com/" title="Icons">
+                A crisp set of 15x15 icons, balanced and consistent.
+              </ListItem>
+            </ul>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Overview</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <ul className="list two">
+              <ListItem
+                title="Introduction"
+                href="/docs/primitives/overview/introduction"
+              >
+                Build high-quality, accessible design systems and web apps.
+              </ListItem>
+              <ListItem
+                title="Getting started"
+                href="/docs/primitives/overview/getting-started"
+              >
+                A quick tutorial to get you up and running with Radix
+                Primitives.
+              </ListItem>
+              <ListItem title="Styling" href="/docs/primitives/guides/styling">
+                Unstyled and compatible with any styling solution.
+              </ListItem>
+              <ListItem
+                title="Animation"
+                href="/docs/primitives/guides/animation"
+              >
+                Use CSS keyframes or any animation library of your choice.
+              </ListItem>
+              <ListItem
+                title="Accessibility"
+                href="/docs/primitives/overview/accessibility"
+              >
+                Tested in a range of browsers and assistive technologies.
+              </ListItem>
+              <ListItem
+                title="Releases"
+                href="/docs/primitives/overview/releases"
+              >
+                Radix Primitives releases and their changelogs.
+              </ListItem>
+            </ul>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Trigger>Disabled</NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <ul className="list two">
+              <ListItem
+                title="Introduction"
+                href="/docs/primitives/overview/introduction"
+              >
+                Build high-quality, accessible design systems and web apps.
+              </ListItem>
+              <ListItem
+                title="Getting started"
+                href="/docs/primitives/overview/getting-started"
+              >
+                A quick tutorial to get you up and running with Radix
+                Primitives.
+              </ListItem>
+              <ListItem title="Styling" href="/docs/primitives/guides/styling">
+                Unstyled and compatible with any styling solution.
+              </ListItem>
+              <ListItem
+                title="Animation"
+                href="/docs/primitives/guides/animation"
+              >
+                Use CSS keyframes or any animation library of your choice.
+              </ListItem>
+              <ListItem
+                title="Accessibility"
+                href="/docs/primitives/overview/accessibility"
+              >
+                Tested in a range of browsers and assistive technologies.
+              </ListItem>
+              <ListItem
+                title="Releases"
+                href="/docs/primitives/overview/releases"
+              >
+                Radix Primitives releases and their changelogs.
+              </ListItem>
+            </ul>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item>
+          <NavigationMenu.Link href="https://github.com/radix-ui">
+            Github
+          </NavigationMenu.Link>
+        </NavigationMenu.Item>
+      </NavigationMenu.Root>
       <Separator className="margin-bt-8" />
 
       <Flex
