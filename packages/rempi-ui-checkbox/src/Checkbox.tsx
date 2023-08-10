8358093bca6c14326b7ddbc43bc3d8fe7a07fdb5
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-08-10 09:05:52 -0300

    feat: add styled component to Checkbox

--- packages/rempi-ui-checkbox/src/Checkbox.tsx
+++ packages/rempi-ui-checkbox/src/Checkbox.tsx
@@ -1,63 +1,58 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { forwardRef, HTMLRempiProps } from "@rempi-ui/core";
 import { Label, LabelContext, LabelProps } from "@rempi-ui/label";
-import classNames from "classnames";
 import { Check } from "lucide-react";
 import { useContext, useEffect } from "react";
-import "./Checkbox.scss";
+import { StyledCheckboxIndicator, StyledCheckboxRoot } from "./Checkbox.styles";
 
 export type CheckboxRootProps = Omit<
-  HTMLRempiProps<typeof Checkbox.Root>,
+  HTMLRempiProps<typeof StyledCheckboxRoot>,
   "as"
 > &
   Checkbox.CheckboxProps &
   Checkbox.CheckboxIndicatorProps;
 
-export const CheckboxRoot = forwardRef<typeof Checkbox.Root, CheckboxRootProps>(
-  ({ children, className, forceMount, asChild, ...props }, ref) => {
-    const { setAttrs: setLabelAttrs } = useContext(LabelContext);
+export const CheckboxRoot = forwardRef<
+  typeof StyledCheckboxRoot,
+  CheckboxRootProps
+>(({ children, forceMount, asChild, ...props }, ref) => {
+  const { setAttrs: setLabelAttrs } = useContext(LabelContext);
 
-    useEffect(() => {
-      setLabelAttrs((currentValue: any) => ({
-        ...currentValue,
-        "data-disabled": props.disabled,
-        "data-invalid": props["aria-invalid"],
-      }));
-    }, [props.disabled, props["aria-invalid"]]);
+  useEffect(() => {
+    setLabelAttrs((currentValue: any) => ({
+      ...currentValue,
+      "data-disabled": props.disabled,
+      "data-invalid": props["aria-invalid"],
+    }));
+  }, [props.disabled, props["aria-invalid"]]);
 
-    useEffect(() => {
-      setLabelAttrs((currentValue: any) => ({
-        ...currentValue,
-        "data-state": props.checked || props.defaultChecked,
-      }));
-    }, []);
+  useEffect(() => {
+    setLabelAttrs((currentValue: any) => ({
+      ...currentValue,
+      "data-state": props.checked || props.defaultChecked,
+    }));
+  }, []);
 
-    function handleOnCheckedChange(checked: boolean | "indeterminate") {
-      props.onCheckedChange?.(checked);
-      setLabelAttrs((currentValue: any) => ({
-        ...currentValue,
-        "data-state": checked,
-      }));
-    }
-
-    return (
-      <Checkbox.Root
-        {...props}
-        onCheckedChange={handleOnCheckedChange}
-        ref={ref}
-        className={classNames("rempi-checkbox__check", className)}
-      >
-        <Checkbox.Indicator
-          className="rempi-checkbox__indicator"
-          forceMount={forceMount}
-          asChild={asChild}
-        >
-          <Check size={12} />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
-    );
+  function handleOnCheckedChange(checked: boolean | "indeterminate") {
+    props.onCheckedChange?.(checked);
+    setLabelAttrs((currentValue: any) => ({
+      ...currentValue,
+      "data-state": checked,
+    }));
   }
-);
+
+  return (
+    <StyledCheckboxRoot
+      {...props}
+      onCheckedChange={handleOnCheckedChange}
+      ref={ref}
+    >
+      <StyledCheckboxIndicator forceMount={forceMount} asChild={asChild}>
+        <Check size={12} />
+      </StyledCheckboxIndicator>
+    </StyledCheckboxRoot>
+  );
+});
 
 // -x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x //
 
