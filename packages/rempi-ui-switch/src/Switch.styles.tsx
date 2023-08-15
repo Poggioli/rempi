ae94157a4716fcbbc13388a70c9f226a5b71c246
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-08-15 09:49:24 -0300

    fix: property name

--- packages/rempi-ui-switch/src/Switch.styles.tsx
+++ packages/rempi-ui-switch/src/Switch.styles.tsx
@@ -2,9 +2,9 @@ import * as Switch from "@radix-ui/react-switch";
 import { styled } from "@rempi-ui/core";
 
 export const StyledSwitchRoot = styled(Switch.Root)`
-  background-color: ${(props) => props.theme.colors.grey[3]};
-  border-color: ${(props) => props.theme.colors.grey[7]};
-  border-radius: ${(props) => props.theme.radiis.pill};
+  background-color: ${(props) => props.theme.colors.grey3};
+  border-color: ${(props) => props.theme.colors.grey7};
+  border-radius: ${(props) => props.theme.radii.pill};
   border-style: solid;
   border-width: ${(props) => props.theme.borderWidths[1]};
   box-sizing: border-box;
@@ -16,30 +16,30 @@ export const StyledSwitchRoot = styled(Switch.Root)`
 
   &:focus,
   &:focus-visible {
-    outline-color: ${(props) => props.theme.colors.info[9]};
+    outline-color: ${(props) => props.theme.colors.info9};
     outline-style: solid;
     outline-width: ${(props) => props.theme.borderWidths[1]};
   }
 
   &[data-state="checked"] {
-    background-color: ${(props) => props.theme.colors.primary[8]};
+    background-color: ${(props) => props.theme.colors.primary8};
   }
 
   &[data-disabled] {
-    border-color: ${(props) => props.theme.colors.grey[6]};
+    border-color: ${(props) => props.theme.colors.grey6};
     cursor: not-allowed;
-    background-color: ${(props) => props.theme.colors.grey[3]};
+    background-color: ${(props) => props.theme.colors.grey3};
 
     &[data-state="checked"] {
-      background-color: ${(props) => props.theme.colors.primary[5]};
+      background-color: ${(props) => props.theme.colors.primary5};
     }
   }
 `;
 
 export const StyledSwitchThumb = styled(Switch.Thumb)`
   display: block;
-  background-color: ${(props) => props.theme.colors.primary[9]};
-  border-radius: ${(props) => props.theme.radiis.pill};
+  background-color: ${(props) => props.theme.colors.primary9};
+  border-radius: ${(props) => props.theme.radii.pill};
   height: calc(
     ${(props) => props.theme.spaces[5]} - ${(props) => props.theme.spaces[1]} /
       2
@@ -53,7 +53,7 @@ export const StyledSwitchThumb = styled(Switch.Thumb)`
   will-change: transform;
 
   &[data-disabled] {
-    background-color: ${(props) => props.theme.colors.primary[7]};
+    background-color: ${(props) => props.theme.colors.primary7};
   }
 
   &[data-state="checked"] {
@@ -63,10 +63,10 @@ export const StyledSwitchThumb = styled(Switch.Thumb)`
           ${(props) => props.theme.spaces[1]} / 2
       )
     );
-    background-color: ${(props) => props.theme.colors.primary[11]};
+    background-color: ${(props) => props.theme.colors.primary11};
 
     &[data-disabled] {
-      background-color: ${(props) => props.theme.colors.primary[8]};
+      background-color: ${(props) => props.theme.colors.primary8};
     }
   }
 `;
