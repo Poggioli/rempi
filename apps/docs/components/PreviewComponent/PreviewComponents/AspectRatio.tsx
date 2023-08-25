4c1d6404887e3bae08ab950a4cdbc6f9d2b2c36b
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-08-25 17:09:42 -0300

    fix: remove class name

--- apps/docs/components/PreviewComponent/PreviewComponents/AspectRatio.tsx
+++ apps/docs/components/PreviewComponent/PreviewComponents/AspectRatio.tsx
@@ -5,7 +5,6 @@ export const AspectRatioPreview: FC = () => {
   return (
     <AspectRatio ratio={16 / 9}>
       <img
-        className="img-aspect-ratio"
         src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
         alt="Landscape photograph by Tobias Tullius"
       />
