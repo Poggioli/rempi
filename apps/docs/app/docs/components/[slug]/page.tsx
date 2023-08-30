6e67d00349854cd7d6e388457fc8a6eaaa6697de
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-08-30 10:54:15 -0300

    feat: using version provider

--- apps/docs/app/docs/components/[slug]/page.tsx
+++ apps/docs/app/docs/components/[slug]/page.tsx
@@ -3,24 +3,32 @@ import { Metadata } from "next";
 import { serialize } from "next-mdx-remote/serialize";
 import { loadMdxFile, readAllFilesInPath } from "utils/loadMdxFile";
 import semverSort from "semver-sort";
+import { VersionProvider } from "@/components/VersionProvider/VersionProvider";
+
+function loadAvailableVersions(slug: string) {
+  const docs: string[] = readAllFilesInPath(
+    `documentations/components/${slug}`
+  );
+  const docsWithoutExt = docs.map((doc) => doc.replace(".mdx", ""));
+  const docsSortedMoreRecentFirst = semverSort.desc(docsWithoutExt);
+
+  return docsSortedMoreRecentFirst;
+}
 
 function loadDocBySlug(slug: string, version?: string) {
-  const docs: string[] = readAllFilesInPath(`documentations/components/${slug}`);
-  const docsWithoutExt = docs.map((doc) => doc.replace(".mdx", ""));
+  const versions = loadAvailableVersions(slug);
 
-  if (version && docsWithoutExt.includes(version)) {
+  if (version && versions.includes(version)) {
     return loadMdxFile(`documentations/components/${slug}`, version);
   }
 
-  const docsSortedMoreRecentFirst = semverSort.desc(docsWithoutExt);
-
-  return loadMdxFile(
-    `documentations/components/${slug}`,
-    docsSortedMoreRecentFirst[0]
-  );
+  return loadMdxFile(`documentations/components/${slug}`, versions[0]);
 }
 
-export async function generateMetadata({ params, searchParams }): Promise<Metadata> {
+export async function generateMetadata({
+  params,
+  searchParams,
+}): Promise<Metadata> {
   const { slug } = params;
   const { v } = searchParams;
 
@@ -37,10 +45,15 @@ async function SlugPage({ params, searchParams }) {
   const { slug } = params;
   const { v } = searchParams;
 
+  const versions = loadAvailableVersions(slug);
   const source = loadDocBySlug(slug, v);
   const content = await serialize(source.content);
 
-  return <MDXWrapper source={content} />;
+  return (
+    <VersionProvider versions={versions}>
+      <MDXWrapper source={content} />
+    </VersionProvider>
+  );
 }
 
 export default SlugPage;
