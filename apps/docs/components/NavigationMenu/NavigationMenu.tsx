25c18cc10832a9358ab05e3c0c640e79b97366d6
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-09-01 09:33:04 -0300

    feat: remove menu

--- apps/docs/components/NavigationMenu/NavigationMenu.tsx
+++ apps/docs/components/NavigationMenu/NavigationMenu.tsx
@@ -1,90 +1,20 @@
 import { SocialMedias } from "@/components/SocialMedias";
-import { IconButton } from "@rempi-ui/button";
-import { Container } from "@rempi-ui/container";
-import { NavigationMenu as RempiNavigationMenu } from "@rempi-ui/navigation-menu";
-import { Popover } from "@rempi-ui/popover";
-import { Collapsible } from "@rempi-ui/collpasible";
-import { Menu } from "lucide-react";
+import { Flex } from "@rempi-ui/flex";
 import { FC } from "react";
 import {
-  StyledNavigationMenuHeader,
-  StyledNavigationMenuHeaderContainer,
-  StyledNavigationMenuHeaderMenuDesktop,
-  StyledNavigationMenuHeaderMenuMobile,
-  StyledNavigationMenuHeaderPopoverContent,
-  StyledNavigationMenuHeaderPopoverTrigger,
+  StyledMarginLeftAuto,
+  StyledNavigationMenuHeader
 } from "./NavigationMenu.styles";
 
 export const NavigationMenu: FC = () => {
   return (
-    <StyledNavigationMenuHeader>
-      <StyledNavigationMenuHeaderContainer
-        as={(props: any) => <Container {...props} as="header" />}
-        variant="md"
-        centered
-      >
-        <Collapsible.Root>
-          <StyledNavigationMenuHeaderPopoverTrigger as={Collapsible.Trigger}>
-            <IconButton
-              size="small"
-              variant="text"
-              aria-label="Menu de navegação"
-            >
-              <Menu size={24} />
-            </IconButton>
-          </StyledNavigationMenuHeaderPopoverTrigger>
-          <StyledNavigationMenuHeaderPopoverContent as={Collapsible.Content}>
-            <StyledNavigationMenuHeaderMenuMobile as={RempiNavigationMenu.Root}>
-              <RempiNavigationMenu.Item>
-                <RempiNavigationMenu.Link href="/">
-                  Rempi-UI
-                </RempiNavigationMenu.Link>
-              </RempiNavigationMenu.Item>
-              <RempiNavigationMenu.Item>
-                <RempiNavigationMenu.Link href="/docs/components/accordion">
-                  Documentation
-                </RempiNavigationMenu.Link>
-              </RempiNavigationMenu.Item>
-              <RempiNavigationMenu.Item>
-                <RempiNavigationMenu.Link href="/components">
-                  Components
-                </RempiNavigationMenu.Link>
-              </RempiNavigationMenu.Item>
-              <RempiNavigationMenu.Item>
-                <RempiNavigationMenu.Link href="/examples">
-                  Examples
-                </RempiNavigationMenu.Link>
-              </RempiNavigationMenu.Item>
-            </StyledNavigationMenuHeaderMenuMobile>
-          </StyledNavigationMenuHeaderPopoverContent>
-        </Collapsible.Root>
-
-        <StyledNavigationMenuHeaderMenuDesktop as={RempiNavigationMenu.Root}>
-          <RempiNavigationMenu.Item>
-            <RempiNavigationMenu.Link href="/">
-              Rempi-UI
-            </RempiNavigationMenu.Link>
-          </RempiNavigationMenu.Item>
-          <RempiNavigationMenu.Item>
-            <RempiNavigationMenu.Link href="/docs/components/accordion">
-              Documentation
-            </RempiNavigationMenu.Link>
-          </RempiNavigationMenu.Item>
-          <RempiNavigationMenu.Item>
-            <RempiNavigationMenu.Link href="/components">
-              Components
-            </RempiNavigationMenu.Link>
-          </RempiNavigationMenu.Item>
-          <RempiNavigationMenu.Item>
-            <RempiNavigationMenu.Link href="/examples">
-              Examples
-            </RempiNavigationMenu.Link>
-          </RempiNavigationMenu.Item>
-        </StyledNavigationMenuHeaderMenuDesktop>
-        <div>
-          <SocialMedias />
-        </div>
-      </StyledNavigationMenuHeaderContainer>
+    <StyledNavigationMenuHeader
+      as={(props: any) => <Flex {...props} as="header" />}
+      justifyContent="space-between"
+    >
+      <StyledMarginLeftAuto>
+        <SocialMedias />
+      </StyledMarginLeftAuto>
     </StyledNavigationMenuHeader>
   );
 };
