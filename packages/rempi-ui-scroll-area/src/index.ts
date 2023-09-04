01f7d07f032633776f06ef8eb498425aac37571a
João Vitor Pogiolli do Lago <joaopogiolli@gmail.com>
2023-09-04 18:56:35 -0300

    feat: add scroll-area component

---
+++ packages/rempi-ui-scroll-area/src/index.ts
@@ -0,0 +1,28 @@
+import {
+  ScrollAreaRootProps,
+  ScrollAreaRoot,
+  ScrollAreaViewportProps,
+  ScrollAreaViewport,
+  ScrollAreaScrollbarProps,
+  ScrollAreaScrollbar,
+  ScrollAreaThumbProps,
+  ScrollAreaThumb,
+  ScrollAreaCornerProps,
+  ScrollAreaCorner,
+} from "./ScrollArea";
+
+export const ScrollArea = {
+  Root: ScrollAreaRoot,
+  Viewport: ScrollAreaViewport,
+  Scrollbar: ScrollAreaScrollbar,
+  Thumb: ScrollAreaThumb,
+  Corner: ScrollAreaCorner,
+};
+
+export type {
+  ScrollAreaRootProps,
+  ScrollAreaViewportProps,
+  ScrollAreaScrollbarProps,
+  ScrollAreaThumbProps,
+  ScrollAreaCornerProps,
+};
